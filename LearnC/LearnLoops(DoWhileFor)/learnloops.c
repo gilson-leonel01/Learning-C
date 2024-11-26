99a1c1443c7b3019579c02c77260290d96ce2774
Gilson Leonel <123027006+gilson-leonel01@users.noreply.github.com>
2024-11-26 12:01:39 +0100

    Update learnloops.c

--- LearnC/LearnLoops(DoWhileFor)/learnloops.c
+++ LearnC/LearnLoops(DoWhileFor)/learnloops.c
@@ -4,6 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int i;
+	for (i = 0; i < 5; i++) {
+	    printf("%d\n", i);
+  	}     
+         
+	  i = 0;
+	  while (i < 5)
+	  {
+	    printf("%d\n", i);
+	    i++;
+	  }     
+         
+	  do {
+	    printf("%d\n", i);
+	    i++;
+	  } while (i < 5);
 	
 	return 0;
 }

298f0a461d30b350aaa0151f06f437d55ae14c49
Gilson Leonel <123027006+gilson-leonel01@users.noreply.github.com>
2024-11-26 12:00:22 +0100

    Update learnc.c

--- LearnC/learnc.c
+++ LearnC/learnc.c
@@ -3,9 +3,47 @@
 
 int main(int argc, char *argv[])
 {
+  int i;       
+  if (i > 0) 
+  {
+    printf("Positivo\n");
+  } else {
+    printf("Negativo ou zero\n");
+  }
+
+  int opcao;       
+  printf("Insira a sua opcao: "\n);
+  scanf("%d", &opcao);
+         
+  switch (opcao) 
+  {
+    case 1:
+        printf("Opção 1\n");
+        break;
+    case 2:
+        printf("Opção 2\n");
+        break;
+    default:
+        printf("Opção inválida\n");
+   }
+         
+  for (i = 0; i < 5; i++) 
+  {
+    printf("%d\n", i);
+  }     
+         
+  i = 0;
+  while (i < 5)
+  {
+    printf("%d\n", i);
+    i++;
+  }     
+         
+  do {
+    printf("%d\n", i);
+    i++;
+  } while (i < 5);
          
   system("PAUSE");	
   return 0;
 }
-
-

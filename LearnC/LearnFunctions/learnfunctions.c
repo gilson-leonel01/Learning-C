546b76f5c4b0eb9eccddd58b190db90751217a97
Gilson Leonel <123027006+gilson-leonel01@users.noreply.github.com>
2024-11-26 11:52:52 +0100

    Update learnfunctions.c

--- LearnC/LearnFunctions/learnfunctions.c
+++ LearnC/LearnFunctions/learnfunctions.c
@@ -3,6 +3,28 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int quadrado(int x) {
+    return x * x;
+}
+
+float media(float a, float b) {
+    return (a + b) / 2;
+}
+
 int main(int argc, char *argv[]) {
+	int num;
+	printf("Insira o valor: "\n);
+	scanf("%d, ", &num);
+
+	quadrado(num);
+
+	float a, b;
+	printf("Insira o valor de a: "\n);
+	scanf("%f, ", &a);
+
+	printf("Insira o valor de b: "\n);
+	scanf("%f, ", &b);
+
+	media(a, b);
 	return 0;
 }

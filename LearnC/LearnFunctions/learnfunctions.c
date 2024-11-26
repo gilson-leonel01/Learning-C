86e5cbad1718b6b695e787a674dc4c5a33b13613
Gilson Leonel <123027006+gilson-leonel01@users.noreply.github.com>
2024-11-26 12:16:47 +0100

    Update learnfunctions.c

--- LearnC/LearnFunctions/learnfunctions.c
+++ LearnC/LearnFunctions/learnfunctions.c
@@ -11,12 +11,38 @@ float media(float a, float b) {
     return (a + b) / 2;
 }
 
+int fatorial(int n) {
+    int resultado = 1;
+    for (int i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+ 
+void verificaParOuImpar(int n) {
+    if (n % 2 == 0) {
+        printf("%d é par.\n", n);
+    } else {
+        printf("%d é ímpar.\n", n);
+    }
+}
+ 
+void contagemRegressiva(int n) {
+    printf("Contagem regressiva:\n");
+    for (int i = n; i >= 0; i--) {
+        printf("%d\n", i);
+    }
+}
+
 int main(int argc, char *argv[]) {
 	int num;
 	printf("Insira o valor: "\n);
 	scanf("%d, ", &num);
 
 	quadrado(num);
+	fatorial(num);
+	verificarParOuImpar(num);
+	contagemRegressiva(num);
 
 	float a, b;
 	printf("Insira o valor de a: "\n);

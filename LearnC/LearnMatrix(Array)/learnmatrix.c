dbda673ee4125133efeb63201c9c5dc1c8556570
Gilson Leonel <123027006+gilson-leonel01@users.noreply.github.com>
2024-11-26 12:12:50 +0100

    Update learnmatrix.c

--- LearnC/LearnMatrix(Array)/learnmatrix.c
+++ LearnC/LearnMatrix(Array)/learnmatrix.c
@@ -4,5 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+   int matriz[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matriz[i][j] = (i + 1) * (j + 1); 
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 	return 0;
 }

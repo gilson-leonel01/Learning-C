64b0c13e225ad94c64713b0890f520acaaa279f0
Gilson Leonel <123027006+gilson-leonel01@users.noreply.github.com>
2024-11-26 12:11:58 +0100

    Update learnc.c

--- LearnC/learnc.c
+++ LearnC/learnc.c
@@ -43,7 +43,30 @@ int main(int argc, char *argv[])
     printf("%d\n", i);
     i++;
   } while (i < 5);
-         
+
+  int vetor[5];
+
+    for (int i = 0; i < 5; i++) {
+        vetor[i] = i + 1; 
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", vetor[i]);
+    }
+  int matriz[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matriz[i][j] = (i + 1) * (j + 1); 
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
   system("PAUSE");	
   return 0;
 }
